7426ddfc4c2aaf62883a80168773da2c94865ecc
Ashlone <ashchibvuri@gmail.com>
2022-04-06 11:59:02 +0200

    2nd commit

---
+++ build/precache-manifest.7fa509ed8b2d1473cfe5ad1e1209b66e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e9e042af3f2cbfcbd098f584c29d88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9168d9d0eebabdfc479a",
+    "url": "/static/css/main.233cbc38.chunk.css"
+  },
+  {
+    "revision": "c23d5974e2945618490f",
+    "url": "/static/js/2.89a5c1db.chunk.js"
+  },
+  {
+    "revision": "9168d9d0eebabdfc479a",
+    "url": "/static/js/main.4bb6731b.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
